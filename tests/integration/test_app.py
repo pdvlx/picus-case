2fd776bb5d47f0c1c95e9160776460868a4447cd
Akın Özen <pdvlxx@gmail.com>
2024-08-12 12:58:48 +0300

    fix: change requirements path

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -3,10 +3,10 @@ import os
 import pytest
 import json
 
-# Add the `src` directory to the Python path
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
-from app import app  # Import after modifying sys.path
+from app import app 
 
 @pytest.fixture
 def client():
